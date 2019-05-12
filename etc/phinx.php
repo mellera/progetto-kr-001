1acbd5cda3b4c20f462d337b61c511e89f9a43c7
Giorgio Mellera <giorgio.mellera@iplusservice.it>
2019-05-12 17:30:21 +0200

    Aggiornato

--- etc/phinx.php
+++ etc/phinx.php
@@ -7,15 +7,7 @@ return [
     'environments' => [
         'default_migration_table' => 'phinxlog',
         'default_database' => 'local',
-        'local' => [
-            'adapter' => 'mysql',
-            'host' => getenv('DB_HOST'),
-            'name' => getenv('DB_NAME'),
-            'user' => getenv('DB_USER'),
-            'pass' => getenv('DB_PASS'),
-            'port' => getenv('DB_PORT'),
-            'charset' => getenv('DB_CHARSET')
-        ]
+        'local' => \Sys\Context::getPhinxConnection()
     ]
 ];
 
